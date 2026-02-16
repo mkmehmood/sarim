5fc3133bb123e28647c0b5023d64870ec38d6088
Mahmood Khan <3mahmoodkhan@gmail.com>
2026-02-16 23:51:10 +0500

    Increment CACHE_NAME to naswar-dealer-v27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'naswar-dealer-v26'; // IMPORTANT: Increment this (v4, v5...) every time you update code!
+const CACHE_NAME = 'naswar-dealer-v27'; // IMPORTANT: Increment this (v4, v5...) every time you update code!
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
